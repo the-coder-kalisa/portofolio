b662d1c9698bd75def44532835d01ffb2a3a890c
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-07-05 23:31:17 +0200

    fixed

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,9 +1,6 @@
-import React from 'react'
-
-function Navigation() {
-  return (
-    <div>Navigation</div>
-  )
-}
-
-export default Navigation
+import React from "react";
+const Navigation: React.FC = () => {
+  const buttons: string[] = ["Home", "About Us", "Services"];
+  return <div className="flex items-center justify-between px-20"></div>;
+};
+export default Navigation;
