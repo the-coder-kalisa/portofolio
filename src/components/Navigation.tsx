a74707f50455bde8c7bf4b8865fa8d0ac20225c8
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-07-06 07:42:03 +0200

    fixed a bug

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "@mui/material";
+
 const Navigation: React.FC = () => {
   const buttons: string[] = ["Home", "About Us", "Services"];
-  return <div className="flex items-center justify-between px-20"></div>;
+  return (
+    <div className="flex items-center justify-between px-20">
+      <Link to="/">Personal</Link>
+      <div className="flex items-center gap-5">
+        {buttons.map((but, index)=>(
+          <Button>{but}</Button>
+        ))}
+      </div>
+    </div>
+  );
 };
 export default Navigation;
