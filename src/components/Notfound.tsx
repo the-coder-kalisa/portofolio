36bab629a22ae61bd642eeede58429dc9d26bc97
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-08-08 22:31:03 +0200

    finished

--- src/components/Notfound.tsx
+++ src/components/Notfound.tsx
@@ -1,14 +1,14 @@
 import { FC } from "react";
-import { useSelector } from "react-redux";
-import { State } from "../store";
+// import { useSelector } from "react-redux";
+// import { State } from "../store";
 
 const NotFound: FC = () => {
-  const { mode } = useSelector<State, State>((state) => state);
+  // const { mode } = useSelector<State, State>((state) => state);
   return (
     <div
-      className={`${
-        mode ? "bg-[#091b2c] text-white" : "bg-white text-black"
-      } flex items-center h-[100vh] justify-center`}
+      // className={`${
+      //   mode ? "bg-[#091b2c] text-white" : "bg-white text-black"
+      // } flex items-center h-[100vh] justify-center`}
     >
       404 This page is not found
     </div>
