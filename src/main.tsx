0a00dcace91365da66ab94c47b2bda3e7c11f86d
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-08-08 22:28:31 +0200

    finished

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,22 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<div>hello</div>}></Route>
+        {/* <Route
+          element={
+            <Provider store={store}>
+              <App />
+            </Provider>
+          }
+          path="/"
+        /> */}
+        <Route
+          element={
+            <Provider store={store}>
+              <NotFound />
+            </Provider>
+          }
+          path="*"
+        />
       </Routes>
     </Router>
   </React.StrictMode>
