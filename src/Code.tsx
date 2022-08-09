7e2d95047b8b0358b2a484a40136da73ea84d5c9
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-08-09 10:40:37 +0200

    added recoil module

---
+++ src/Code.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import { RecoilRoot } from "recoil";
+import App from "./App";
+
+const Code : FC = () => {
+    return(
+        <RecoilRoot>
+            <App />
+        </RecoilRoot>
+    )
+}
+export default Code;
--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NotFound from "./components/Notfound";
-import { RecoilRoot } from "recoil";
+import Code from "./Code";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route
-          element={
-            <RecoilRoot>
-              <App />
-            </RecoilRoot>
-          }
-          path="/"
-        />
+        <Route element={<Code />} path="/" />
         <Route element={<NotFound />} path="*" />
       </Routes>
     </Router>
