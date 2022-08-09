d10a0a89a0392cea10b0785fb410bd71c8d674f2
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-08-09 11:34:29 +0200

    added recoil module

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -52,9 +52,9 @@ const Navigation: React.FC = () => {
                     {button}
                   </a>
                 );
-              })} */}
+              })}
             </div>
-          )}
+          )} */}
         </div>
       )}
       <Link
