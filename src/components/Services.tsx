b398a5db43c90370a03917b8baf053e3e9e5ccf7
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-11-02 11:44:47 +0200

    made resume for my portofolio

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -2,7 +2,7 @@ import { Button } from "@mui/material";
 import React, { useState, useEffect } from "react";
 import { categorie, knowledge } from "../data";
 import { transition } from "../data";
-import CV from "../images/KALISA INEZA Giovanni CV.png";
+import CV from "../images/kalisa.pdf";
 import { Close } from "@mui/icons-material";
 import { useRecoilValue } from "recoil";
 import { fonts, modes, phones } from "../atom";
